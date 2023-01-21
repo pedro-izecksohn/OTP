a4297e924edc7a2c722545e3cd6a12f2ad55ac45
Pedro Izecksohn <izecksohn@yahoo.com>
2023-01-21 20:55:18 -0300

    Now it works with UTF-8.

--- OTP.py
+++ OTP.py
@@ -5,17 +5,17 @@ import sys
 random.seed(int(input("Enter the seed: ")))
 ed = input("Enter e to encrypt or d to decrypt: ")
 if ed=="e":
-    plain=input("Enter the text: ")
+    plain=bytes(input("Enter the text: "),"UTF-8")
     lp=len(plain)
     key=random.randbytes(lp)
     lpmo=lp-1
     for i,c in enumerate(plain):
-        sys.stdout.write(str(ord(plain[i])^key[i])+("," if i<lpmo else ""))
+        sys.stdout.write(str(plain[i]^key[i])+("," if i<lpmo else ""))
 elif ed=="d":
     allnumbers = input("Enter the numbers: ").split(",")
     key=random.randbytes(len(allnumbers))
     for i,s in enumerate(allnumbers):
         allnumbers[i]=int(s)^key[i]
-    print(bytes(allnumbers).decode("ASCII"))
+    print(bytes(allnumbers).decode("UTF-8"))
 else:
     print("Unrecognized option.")
